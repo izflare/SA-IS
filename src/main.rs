a69040445dc95f2716bbb5eb7c9efd1c239dfde7
flare <izflare@protonmail.com>
2019-02-06 15:24:44 +0900

    minor fix

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,30 @@
 extern crate sais;
+extern crate clap;
 
-use std::env;
+use clap::{App, Arg};
 use std::io::{prelude::*, BufReader};
 use std::fs::File;
 use std::time::Instant;
 use sais::suffixarray;
 
-fn usage(s: &str) -> () {
-    println!("Usage: {} <filename>", s);
-}
-
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    if (&args).len() != 2 {
-        usage(&args[0]);
-        return;
-    }
+    let app = App::new("sais")
+        //{{{
+        .version("0.1.0")                       
+        .author("flare")     
+        .about("linear time suffix array constructor")
+        .arg(Arg::with_name("input")
+            .help("filename of input sourse text")
+            .short("i")
+            .long("input")
+            .takes_value(true)
+            .required(true)
+        );
+        //}}}
+    let matches = app.get_matches();
+
     let mut s = String::new();
-    let mut f = BufReader::new(File::open(&args[1]).expect("file not found"));
+    let mut f = BufReader::new(File::open(&matches.value_of("input").unwrap()).expect("file not found"));
     f.read_to_string(&mut s).unwrap();
 
     let mut v: Vec<usize> = s.chars().map(|c| c as usize).collect();
